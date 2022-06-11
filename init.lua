c59fda828c5f5478f61b86fca2153d94d79b8a85
金勝紀 <kim@kimbp.local>
2022-06-11 17:49:17 +0900

    add setting

--- init.lua
+++ init.lua
@@ -66,37 +66,3 @@ vim.o.ignorecase = true
 vim.o.smartcase = true--大文字がある場合区別する
 
 
---ステータスラインのカスタマイズ
-local lualine = require('lualine')
-lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'gruvbox_material',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {
-      {
-    'filename',
-    'g:coc_status'
-      }
-    },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
-}
